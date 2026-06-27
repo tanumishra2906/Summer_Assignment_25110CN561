66f60b478906a014a9aebcef58355f89cf76dfce
tanumishra2906 <25110cn561@glbitm.ac.in>
2026-06-27 14:20:05 +0530

    modified ques 91

--- day 23/ques91.c
+++ day 23/ques91.c
@@ -3,6 +3,7 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 int main(){
     char str1[100], str2[100] ;
     int i,j ,count[256]={0}; //rather than taking diff count var for each letter consider an array of count which can contain max 256 boxes cuz utne hi characters
@@ -16,11 +17,9 @@ int main(){
     str1[strcspn(str1, "\n")] = '\0';
     str2[strcspn(str2, "\n")] = '\0'; //this because fgets also stores newline
 
-
-
     for(i=0;str1[i]!='\0' ;i++){
         if(str1[i]!=' '){
-            count[str1[i]]++;
+            count[tolower(str1[i])]++;
 
         }
         //look there are boxes for each letter..supposse letter s ka count from s1 is +1 then iss letter ka count from s2 is -1 net count 0 for that letter
@@ -30,11 +29,9 @@ int main(){
     }
     for(i=0;str2[i]!='\0' ;i++){
         if(str2[i]!=' '){
-            count[str2[i]]--;
+            count[tolower(str2[i])]--;
 
         }
-        
-
     }
     
     for(int i=0;i<256;i++){
